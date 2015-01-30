6ad85313caabbc5d66d1ae769c19c509c48f4a1a
Josh Sloat <jsloat@articulate.com>
2015-01-30 15:49:22 -0500

    Added a WatchKit target and a workspace.

---
+++ WatchHackathon/WatchHackathon WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  WatchHackathon WatchKit Extension
+//
+//  Created by Josh Sloat on 1/30/15.
+//  Copyright (c) 2015 Nimble Guerilla. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ WatchHackathon/WatchHackathon/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WatchHackathon
+//
+//  Created by Josh Sloat on 1/30/15.
+//  Copyright (c) 2015 Nimble Guerilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
